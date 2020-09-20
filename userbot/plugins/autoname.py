3e470748e2e539ac9a6a895548252b0dce4f1d66
Zohre27 <64759017+Zohre27@users.noreply.github.com>
2020-09-20 20:37:27 +0430

    Update autoname.py

--- userbot/plugins/autoname.py
+++ userbot/plugins/autoname.py
@@ -23,7 +23,7 @@ async def _(event):
 
         HM = time.strftime("%H:%M")
 
-        name = f"⌚{HM} 🎀{DEFAULTUSER(zohre)}🎀 📆{DM}"
+        name = f"⌚{HM} 🎀{DEFAULTUSER}🎀 📆{DM}"
 
         logger.info(name)
 
